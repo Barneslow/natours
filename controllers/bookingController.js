a6e0a183c268040ad5ef104b7ee5981ef277804d
barneslow <darrachb1991@gmail.com>
2022-03-10 00:01:28 +0000

    added Readme

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -45,8 +45,6 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
   await Booking.create({ tour, user, price });
 
   res.redirect(req.originalUrl.split('?')[0]);
-
-  next();
 });
 
 exports.createBooking = factory.createOne(Booking);
